1a784693e6508819acd5250cfd865650abba64c4
samat <samat020404@gmail.com>
2023-04-04 01:46:24 +0800

    added copy button

--- js/script.js
+++ js/script.js
@@ -5,54 +5,54 @@ const COLORS = [
         text: '#5a21cd',
     },
     {
-    	hex: '#DC2F02',
-    	rgb: 'rgb(220, 47, 2)',
-    	text: '#fff'
+        hex: '#DC2F02',
+        rgb: 'rgb(220, 47, 2)',
+        text: '#fff',
     },
     {
-    	hex: '#1D3557',
-    	rgb: 'rgb(29, 53, 87)',
-    	text: '#fff'
+        hex: '#1D3557',
+        rgb: 'rgb(29, 53, 87)',
+        text: '#fff',
     },
     {
-    	hex: '#00B4D8',
-    	rgb: 'rgb(0, 180, 216)',
-    	text: '#e63946'
+        hex: '#00B4D8',
+        rgb: 'rgb(0, 180, 216)',
+        text: '#e63946',
     },
     {
-    	hex: '#ffffff',
-    	rgb: 'rgb(255, 255, 255)',
-    	text: '#000000'
+        hex: '#ffffff',
+        rgb: 'rgb(255, 255, 255)',
+        text: '#000000',
     },
     {
-    	hex: '#9D4EDD',
-    	rgb: 'rgb(157, 78, 221)',
-    	text: '#95d5b2'
+        hex: '#9D4EDD',
+        rgb: 'rgb(157, 78, 221)',
+        text: '#95d5b2',
     },
     {
-    	hex: '#38B000',
-    	rgb: 'rgb(56, 176, 0)',
-    	text: '#450920'
+        hex: '#38B000',
+        rgb: 'rgb(56, 176, 0)',
+        text: '#450920',
     },
     {
-    	hex: '#9C6644',
-    	rgb: 'rgb(156, 102, 68)',
-    	text: '#023e7d'
+        hex: '#9C6644',
+        rgb: 'rgb(156, 102, 68)',
+        text: '#023e7d',
     },
     {
-    	hex: '#7371FC',
-    	rgb: 'rgb(115, 113, 252)',
-    	text: '#80FFDB'
+        hex: '#7371FC',
+        rgb: 'rgb(115, 113, 252)',
+        text: '#80FFDB',
     },
     {
-    	hex: '#4CC9F0',
-    	rgb: 'rgb(76, 201, 240)',
-    	text: '#480ca8'
+        hex: '#4CC9F0',
+        rgb: 'rgb(76, 201, 240)',
+        text: '#480ca8',
     },
     {
-    	hex: '#000000',
-    	rgb: 'rgb(0, 0, 0)',
-    	text: '#ffffff'
+        hex: '#000000',
+        rgb: 'rgb(0, 0, 0)',
+        text: '#ffffff',
     },
 ];
 const span = document.querySelectorAll('.span');
@@ -80,6 +80,10 @@ container.addEventListener('click', () => {
     buttonRGB.style.color = randomColor.text;
 
     container.style.backgroundColor = randomColor[currentColorFormat];
+    colorName.addEventListener('click', () => {
+        const textToCopy = randomColor[currentColorFormat];
+        navigator.clipboard.writeText(textToCopy);
+    });
 });
 
 buttonHEX.addEventListener('click', () => {
@@ -93,4 +97,3 @@ buttonRGB.addEventListener('click', () => {
         currentColorFormat = 'rgb';
     }
 });
-
